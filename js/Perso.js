afeb6e687117c50cd00772acded6f6caae2ad257
Brieux "Thazerr" Caquelin <33527111+Brieux@users.noreply.github.com>
2020-12-17 09:21:51 +0100

    changement parole

--- js/Perso.js
+++ js/Perso.js
@@ -3,15 +3,15 @@ class Perso extends ElementHtml{
         super($html);
         this.$bulle=$html.find(".bulle");
         this.mots=[
-            "Bof"
-            ,"On s'emmerde un peu"
-            ,"Fait chaud"
-            ,"..."
-            ,"Rolland Garros c'est mieux"
-            ,"zyva l'autre avec sa raquette"
-            ,"sont pas doués"
-            ,"Ya koi ce soir à la télé?"
-            ,"vive moi!"
+            "J'aime utiliser mon cerveau"
+            ,"Aujourd'hui est un jour spécial. Indice: c'est mon anniversaire..."
+            ,"C'est toujours un plaisir de vous voir, sosie"
+            ,"Je déteste les chiffres"
+            ,"J'aime aussi bien recevoir des cadeaux qu'en donner."
+            ,"Vivement l'hiver..."
+            ,"J'aime étudier et enrichir mes connaissances."
+            ,"On voit passer des avions, parfois. Je me demande bien d'où ils viennent..."
+            ,"J'ai l'impression que je suis né pour accomplir de grandes choses"
         ];
     }
     parle(blabla){
